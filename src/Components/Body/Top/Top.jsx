4d805e435bcde8832d7b8c147f0848079f1a172f
odumade <104792577+odumade@users.noreply.github.com>
2022-12-30 16:11:04 +0100

    completed top component

--- src/Components/Body/Top/Top.jsx
+++ src/Components/Body/Top/Top.jsx
@@ -5,8 +5,12 @@ import './top.css'
 import {BiSearch} from "react-icons/bi";
 import {MdOutlineNotificationsNone} from "react-icons/md";
 import {TbMessageCircle} from "react-icons/tb";
+import {BsArrowRightShort, BsQuestionCircle} from "react-icons/bs";
 
 import img from "../../../assets/img.jpg"
+import img2 from "../../../assets/img2.jpg"
+import video from "../../../assets/Grass.mp4"
+
 
 function Top() {
   return (
@@ -35,6 +39,68 @@ function Top() {
 
       </div>
 
+      <div className="cardSection flex">
+
+        <div className="rightCard flex">
+          <h1>Create and sell extraordinary products</h1>
+          <p>The world's fast growing industry today are natural made produt!</p>
+
+          <div className="buttons flex">
+            <button className="btn">Explore More</button>
+            <button className="btn transparent">Top Sellers</button>
+          </div>
+
+          <div className="videoDiv">
+            <video src={video} autoPlay loop muted></video>
+          </div>
+        </div>
+
+        <div className="leftCard flex">
+          <div className="main flex">
+
+            <div className="textDiv">
+              <h1>My Stat</h1>
+
+              <div className="flex">
+                <span>
+                  Today <br/> <small>4 Orders</small>
+                </span>
+              </div>
+
+              <div className="flex">
+                <span>
+                  This month <br/> <small>175 Orders</small>
+                </span>
+              </div>
+
+              <span className="flex link">
+                Go to my orders <BsArrowRightShort className="icon" />
+              </span>
+
+            </div>
+
+            <div className="imgDiv">
+              <img src={img2} alt="Image Name"/>
+            </div>
+
+            {/*
+            <div className="sideBarCard">
+              <BsQuestionCircle className="icon" />
+              <div className="cardContent">
+                <div className="circle1"></div>
+                <div className="circle2"></div>
+
+                <h3>Help Center</h3>
+                <p>Having trouble with Buddy please contact us for more questions</p>
+                <button className="btn">Go to help center</button>
+              </div>
+            </div>
+            */}
+
+          </div>
+        </div>
+
+      </div>
 
     </div>
   )
