e59d9f15fca820b0b59697a22c67de47b7d61268
odumade <104792577+odumade@users.noreply.github.com>
2022-12-30 03:40:35 +0100

    set up the sidebar layout

--- src/Components/Side/Sidebar.jsx
+++ src/Components/Side/Sidebar.jsx
@@ -1,10 +1,121 @@
 import React from 'react'
 import './sidebar.css'
 
+import logo from '../../assets/logo.png'
+import logo2 from '../../assets/logo2.png'
+
+
+import {IoMdSpeedometer} from "react-icons/io";
+import {MdDeliveryDining, MdOutlineExplore, MdOutlinePermContactCalendar} from "react-icons/md";
+import {BsCreditCard2Front, BsTrophy} from "react-icons/bs";
+import {AiOutlinePieChart} from "react-icons/ai";
+import {BiTrendingUp} from "react-icons/bi";
+
+
 function Sidebar() {
     return (
-        <div>
-            Sidebar
+        <div className="sideBar grid">
+            <div className="logoDiv flex">
+                <img src={logo} alt="logo"/>
+            </div>
+
+            <div className="menuDiv">
+                <h3 className="divTitle">
+                    QUICK MENU
+                </h3>
+                <ul className="menuLists grid">
+
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <IoMdSpeedometer className="icon" />
+                            <span className="smalltext">
+                                Dash board
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdDeliveryDining className="icon" />
+                            <span className="smalltext">
+                                My Orders
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdOutlineExplore className="icon" />
+                            <span className="smalltext">
+                                Explore
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <BsTrophy className="icon" />
+                            <span className="smalltext">
+                                Product
+                            </span>
+                        </a>
+                    </li>
+
+
+
+
+                </ul>
+            </div>
+
+            <div className="settingsDiv">
+                <h3 className="divTitle">
+                    SETTINGS
+                </h3>
+                <ul className="menuLists grid">
+
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <AiOutlinePieChart className="icon" />
+                            <span className="smalltext">
+                                Chart
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <BiTrendingUp className="icon" />
+                            <span className="smalltext">
+                                Trends
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdOutlinePermContactCalendar className="icon" />
+                            <span className="smalltext">
+                                Contact
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <BsCreditCard2Front className="icon" />
+                            <span className="smalltext">
+                                Billing
+                            </span>
+                        </a>
+                    </li>
+
+
+
+
+                </ul>
+            </div>
+
+
         </div>
     )
 }
